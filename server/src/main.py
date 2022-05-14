3b68e48c4662590413b05c99bd68abcfae676cdd
Alexander Brown <A.r.brown@student.tudelft.nl>
2022-05-14 16:17:59 +0200

    add basic application

--- server/src/main.py
+++ server/src/main.py
@@ -5,15 +5,16 @@ import api
 con = duckdb.connect()
 con.execute("CREATE TABLE tbl AS SELECT * FROM read_csv_auto('data.csv', header = True, columns = {'Votes': 'INTEGER', 'Movie': 'String'})")
 
-
-api.vote(con, "Jungkook")
-api.vote(con, "Junkook")
-
-
-# output the database to the csv
-con.execute("COPY tbl TO 'data.csv' (HEADER, DELIMITER ',')")
-con.close()
-
-
-
+while True:
+    print("Please choose an option:\n1:vote\n2:quit")
+    option = input()
+    if option == "1":
+        print("You have chosen to vote for jungkookc")
+        api.vote(con, "Jungkook")
+    elif option == "2":
+        con.execute("COPY tbl TO 'data.csv' (HEADER, DELIMITER ',')")
+        con.close()
+        break
+    else:
+        print("invalid input")
 
